a7465070e949433314bd21ea3f1eb259af9a70a1
John Crowley <johncrowleydev@gmail.com>
2021-08-28 14:09:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SpongeBobGenerator
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("SpOnGeBoB tExT gEnErAtOr v1.0.0\nby John Crowley");
+            Console.ResetColor();
+
+            while (true) {
+
+            Console.WriteLine("\nEnter text: ");
+
+            string input = Console.ReadLine();
+
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(char.ToUpper(input[i]));
+                }
+                else {
+                    Console.Write(input[i]);
+                }
+            }
+
+                Console.ResetColor();
+
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
